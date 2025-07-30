40596db2f175e7d631de694e674bc1f6e5518c4b
Cesar Raul Valdivia Huayllas <cesarhuayllas016@gmail.com>
2025-07-30 02:55:06 -0500

    Implementacion funcion sorteaAmigo()

--- app.js
+++ app.js
@@ -26,3 +26,19 @@ function actualizarAmigos()
         document.getElementById('listaAmigos').innerHTML += `<li>${listaAmigos[i]}</li>`;
     }
 }
+
+function sortearAmigo()
+{
+    let numeroAmigo = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSeleccionado = listaAmigos[numeroAmigo];
+    
+    if(listaAmigos.length == 0)
+    {
+        alert("Ingrese amigos para sortear.");
+    }
+
+    else
+    {
+        document.getElementById('resultado').innerHTML = amigoSeleccionado;
+    }
+}
